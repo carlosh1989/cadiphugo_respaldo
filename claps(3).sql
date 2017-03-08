ab149b2172debb6eb78c29cc7849d139c0eaa05d
Od@ <elmorochez22@gmail.com>
2017-03-08 15:50:59 -0400

    asdas

--- claps(3).sql
+++ claps(3).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
+-- version 4.2.12deb2+deb8u1
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 05-03-2017 a las 17:00:17
--- Versión del servidor: 5.7.13-0ubuntu0.16.04.2
--- Versión de PHP: 5.6.28-1+deb.sury.org~xenial+1
+-- Tiempo de generación: 08-03-2017 a las 15:50:14
+-- Versión del servidor: 5.5.49-0+deb8u1
+-- Versión de PHP: 5.6.27-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `CADIP2`
@@ -26,19 +26,19 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `claps`
 --
 
-CREATE TABLE `claps` (
-  `id` int(255) NOT NULL,
-  `id_estado` int(255) NOT NULL,
-  `id_municipio` int(255) NOT NULL,
-  `id_parroquia` int(255) NOT NULL,
-  `codigo_clap` varchar(50) NOT NULL,
-  `nombre_clap` varchar(200) NOT NULL,
+CREATE TABLE IF NOT EXISTS `claps` (
+`id` int(255) NOT NULL,
+  `estado_id` int(255) NOT NULL,
+  `municipio_id` int(255) NOT NULL,
+  `parroquia_id` int(255) NOT NULL,
+  `clap_codigo` varchar(50) NOT NULL,
+  `clap_nombre` varchar(200) NOT NULL,
   `comunidad` varchar(100) NOT NULL,
-  `cargo` varchar(50) NOT NULL,
+  `cargo_id` int(255) NOT NULL,
   `tipo` varchar(10) NOT NULL,
   `cedula` int(10) NOT NULL,
   `nombre_apellido` varchar(100) NOT NULL,
-  `telefono` varchar(50) NOT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
   `registrado` varchar(100) NOT NULL,
   `ubicado` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -51,7 +51,7 @@ CREATE TABLE `claps` (
 -- Indices de la tabla `claps`
 --
 ALTER TABLE `claps`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -61,7 +61,7 @@ ALTER TABLE `claps`
 -- AUTO_INCREMENT de la tabla `claps`
 --
 ALTER TABLE `claps`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
